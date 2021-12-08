064a6711ab99b3ac41a1f766190d85ec55c1625c
kristjanb <kristjan.bleiweis@gmail.com>
2021-12-08 10:42:11 +0100

    Changing values when clicked

--- logic.js
+++ logic.js
@@ -1,13 +1,13 @@
 class Slider {
 
-    constructor(id, x, y, name, min, max, value) {
+    constructor(id, x, y, name, min, max, amount) {
         this.id = id;
         this.x = x;
         this.y = y;
         this.name = name;
         this.min = min;
         this.max = max;
-        this.value = value;
+        this.amount = amount;
     }
 }
 
@@ -39,6 +39,19 @@ function getMousePos(canvas, evt) {
 function drawSlider(x, y, r, percentage, color) {
     var ctx = canvas.getContext("2d");
 
+    ctx.lineWidth = 1;
+
+    ctx.beginPath();
+    ctx.strokeStyle = '#000000';
+    ctx.arc(x, y, r + 7, 0, 2 * Math.PI);
+    ctx.stroke();
+
+    ctx.beginPath();
+    ctx.strokeStyle = '#000000';
+    ctx.arc(x, y, r - 7, 0, 2 * Math.PI);
+    ctx.stroke();
+
+
     ctx.lineWidth = 15;
 
     ctx.strokeStyle = color;
@@ -61,6 +74,17 @@ function drawSlider(x, y, r, percentage, color) {
     }
 }
 
+function drawThemAll() {
+    sliders.forEach(slider => {
+        drawSlider(
+            slider.x,
+            slider.y,
+            (slider.id + 1) * 40,
+            (slider.amount / slider.max),
+            colors[slider.id]);
+    })
+}
+
 // Vsakic ko premaknem misko se tole aktivira
 canvas.addEventListener('mousemove', function (evt) {
     var mousePos = getMousePos(canvas, evt);
@@ -68,24 +92,110 @@ canvas.addEventListener('mousemove', function (evt) {
     var message = 'Mouse position: ' + mousePos.x + ',' + mousePos.y + " | " + amount;
     writeMessage(canvas, message);
 
-    sliders.forEach(slider => {
-        drawSlider(
-            slider.x,
-            slider.y,
-            (slider.id + 1) * 40,
-            (slider.value / slider.max),
-            colors[slider.id]);
-    })
+    drawThemAll();
 
 
 }, false);
 
-document.getElementById("btn_add").addEventListener("click", function() {
+function abs(x) {
+    return x < 0 ? -x : x;
+}
+
+// Vsakic ko premaknem misko se tole aktivira
+canvas.addEventListener('click', function (evt) {
+    var mousePos = getMousePos(canvas, evt);
+    let x = mousePos.x;
+    let y = mousePos.y;
+
+
+    let xx = abs(mousePos.x - 250);
+    let yy = abs(mousePos.y - 250);
+
+
+
+    let r = Math.sqrt(xx * xx + yy * yy);
+
+
+    // r je pravilno izračunan
+
+    for (let i = 1; i < sliders.length + 1; i++) {
+        if (abs((i * 40) - r) < 6) {
+            console.log("-----------------------------------------------");
+            console.log("xx: " + xx);
+            console.log("yy: " + yy);
+            console.log(r);
+            console.log("zadel si " + i + " trail");
+
+            console.log(sliders[0])
+
+            let kvadrant = 0;
+            let kot = 0;
+
+            if (x > 250 && y < 250) {
+                kvadrant = 1;
+            } else if (x < 250 && y < 250) {
+                kvadrant = 2;
+            } else if (x < 250 && y > 250) {
+                kvadrant = 3;
+            } else if (x > 250 && y > 250) {
+                kvadrant = 4;
+            } else {
+                console.log("i cannot find this quadrant");
+            }
+
+            kot = Math.asin(yy / r);
+
+            kot = kot * 180 / Math.PI;
+            switch (kvadrant) {
+                case 0:
+                    break;
+                case 1:
+                    console.log("si v 1 kvadrantu ");
+                    console.log("kot je : " + (90 - kot));
+                    sliders[i - 1].amount = (0.25 * ((90 - kot) / 90) * sliders[i - 1].max);
+                    break;
+
+                case 2:
+                    console.log("si v 2 kvadrantu ");
+                    console.log("kot je : " + kot);
+
+                    sliders[i - 1].amount = (0.75 * sliders[i - 1].max) + (0.25 * (kot / 90) * sliders[i - 1].max);
+
+
+                    break;
+
+                case 3:
+                    console.log("si v 3 kvadrantu");
+                    console.log("kot je : " + kot);
+
+                    sliders[i - 1].amount = (0.5 * sliders[i - 1].max) + (0.25 * ((90 - kot) / 90) * sliders[i - 1].max);
+                    break;
+
+                case 4:
+                    console.log("si v 4 kvadrantu");
+                    console.log("kot je : " + kot);
+
+                    sliders[i - 1].amount = (0.25 * sliders[i - 1].max) + (0.25 * (kot / 90) * sliders[i - 1].max);
+                    break;
+
+            }
+
+        }
+    }
+
+    drawThemAll();
+
+
+}, false);
+
+document.getElementById("btn_add").addEventListener("click", function () {
 
     let minValue = document.getElementById("min_value").value;
     let maxValue = document.getElementById("max_value").value;
-    let value = document.getElementById("value").value;
+    let amount = document.getElementById("value").value;
 
     sliders.push(
-        new Slider(sliders.length, 250, 250, 'name', minValue, maxValue, value));
-  });
+        new Slider(sliders.length, 250, 250, 'name', minValue, maxValue, amount));
+
+    drawThemAll();
+});
